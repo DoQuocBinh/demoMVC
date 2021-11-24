1c0a4ed19cccca1b27ea175508a6f30114de5374
DoQuocBinh <binhdq@fpt.edu.vn>
2021-11-24 10:20:18 +0700

    add databaseHandler

--- admin.js
+++ admin.js
@@ -1,17 +1,28 @@
 const express = require('express')
+const { insertObject } = require('./databaseHandler')
 const router = express.Router()
 
 router.get('/',(req,res)=>{
     res.render('adminIndex')
 })
 
-router.get('/addStaff',(req,res)=>{
-    res.render('addStaff')
+router.get('/addUser',(req,res)=>{
+    res.render('addUser')
+})
+//Submit add User
+router.post('/addUser',(req,res)=>{
+    const name = req.body.txtName
+    const role = req.body.Role
+    const pass= req.body.txtPassword
+    const objectToInsert = {
+        userName: name,
+        role:role,
+        password: pass
+    }
+    insertObject("Users",objectToInsert)
+    res.render('adminIndex')
 })
 
-router.get('/addTrainer',(req,res)=>{
-    res.render('addTrainer')
-})
 
 module.exports = router;
 
